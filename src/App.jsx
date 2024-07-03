5b9f42471ef48007abb77d6679180443ea5c69d2
Nirmal Tamang <lamanirmal27@gmail.com>
2024-07-03 17:16:00 +0545

    add: home Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/home";
 import LoginPage from "./pages/login";
 import Register from "./pages/register/Register";
 import Header from "./component/header/navbar";
+import ProductList from "./component/ProductList";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       {/* <Sidebar /> */}
       <Header />
       <Routes>
-        {/* <Route exact path="/" element={<Home />} /> */}
+        <Route exact path="/" element={<ProductList/>} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<Register />} />
       </Routes>
