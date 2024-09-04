18188a35cebbe78f8e31ab0096fc4077463ad0a4
Nirmal Tamang <lamanirmal27@gmail.com>
2024-09-04 11:50:03 +0545

    deploy pages

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,38 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes("node_modules")) {
+            // Split MUI components into separate chunks
+            if (id.includes("@mui/material")) {
+              return "mui-material-vendor";
+            }
+            if (id.includes("@mui/icons-material")) {
+              return "mui-icons-vendor";
+            }
+            if (id.includes("@mui/x-date-pickers")) {
+              return "mui-date-pickers-vendor";
+            }
+            // Grouping React-related libraries together
+            if (id.includes("react")) {
+              return "react-vendor";
+            }
+            // Grouping utility libraries together
+            if (
+              id.includes("axios") ||
+              id.includes("moment-timezone") ||
+              id.includes("shortid") ||
+              id.includes("uuid")
+            ) {
+              return "utility-vendor";
+            }
+            // Grouping Tailwind-related libraries together
+            if (id.includes("tailwindcss") || id.includes("@headlessui")) {
+              return "tailwind-vendor";
+            }
+            // Grouping icon libraries together
+            if (id.includes("react-icons") || id.includes("@heroicons")) {
+              return "icon-vendor";
+            }
+            // Default behavior: create a separate chunk for other node_modules
             return id
               .toString()
               .split("node_modules/")[1]
