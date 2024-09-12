7b440603a543baacf90bbcc4f8e6f2d2fc9dfca4
Nirmal Tamang <lamanirmal27@gmail.com>
2024-09-12 06:45:52 +0545

    UI enhanced

--- src/pages/product/ProductList.jsx
+++ src/pages/product/ProductList.jsx
@@ -97,7 +97,7 @@ export default function ProductList() {
           </span>
 
           <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            {newItem.slice(0, all ? newItem.length : 8).map(
+            {newItem.slice(0, all ? newItem.length : 11).map(
               (product) =>
                 product.category === "fashion" && (
                   <a
@@ -154,7 +154,7 @@ export default function ProductList() {
           </span>
 
           <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            {newItem.slice(0, all1 ? newItem.length : 28).map(
+            {newItem.slice(21, all1 ? newItem.length : 33).map(
               (product) =>
                 product.category === "Electronics" && (
                   <a
@@ -168,7 +168,6 @@ export default function ProductList() {
                     <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                       <img
                         src={product.images[0].src}
-                        // alt={product.imageAlt}
                         className="h-full w-full object-cover object-center group-hover:opacity-75"
                       />
                     </div>
