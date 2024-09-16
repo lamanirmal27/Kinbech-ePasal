4653dee7b3ef73594f01173ff0743fbe2990bf5d
Nirmal Tamang <lamanirmal27@gmail.com>
2024-09-16 16:15:35 +0545

    finally done

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -38,7 +38,7 @@ const handleLogin = async (req, res) => {
     res.cookie("jwt", refreshToken, {
       httpOnly: true,
       sameSite: "None",
-      secure: false,
+      secure: true,
       maxAge: 24 * 60 * 60 * 1000,
     });
     res.json({
